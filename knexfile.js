c946fe60eeb4885503e59c3e495cdcfdae0ef518
jerry <jphuong91@gmail.com>
2021-04-07 22:33:22 +0200

    added production environment

--- knexfile.js
+++ knexfile.js
@@ -9,35 +9,16 @@ module.exports = {
     useNullAsDefault: true,
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
       tableName: 'knex_migrations',
+      directory: './migrations',
     },
   },
 };
